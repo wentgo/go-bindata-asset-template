b009628e37695e9a6383d56736aefa962785b4a2
Hanson Li <lihsca@gmail.com>
2019-10-22 12:54:37 -0400

    use chi framework

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
 	"time"
+
+	"github.com/go-chi/chi"
 )
 
 var tpl *template.Template
@@ -14,16 +17,42 @@ func init() {
 }
 
 func main() {
-	http.HandleFunc("/", logging(index))
-	http.HandleFunc("/user/login", logging(userLogin))
-	http.HandleFunc("/error/404", logging(error404))
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
-	http.Handle("/favicon.ico", http.NotFoundHandler())
+	r := chi.NewRouter()
+
+	//r.Use(middleware.Logger)
+	r.Use(logging)
+
+	r.Get("/", index)
+	r.Get("/user/login", userLogin)
+	r.Get("/error/404", error404)
+	r.NotFound(error404)
+
+	//http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	FileServer(r, "/assets", http.Dir("assets"))
+
 	fmt.Println("Listening on localhost:8080 ...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r)
 }
 
-func logging(next http.HandlerFunc) http.HandlerFunc {
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
+}
+
+func logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(time.Now().Format("2006-01-02 03:04:05"), r.RemoteAddr, r.Method, r.URL)
 		next.ServeHTTP(w, r)
@@ -31,22 +60,15 @@ func logging(next http.HandlerFunc) http.HandlerFunc {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		//http.Redirect(w, r, "/", http.StatusSeeOther)
-		//http.Redirect(w, r, "/error/404", 301)
-		//http.NotFound(w, r)
-		error404(w, r)
-		return
-	}
 	data := make(map[string]interface{})
 	data["Title"] = "Index"
-	data["User"] = "Sam Wang"
 	tpl.ExecuteTemplate(w, "home-index.gohtml", data)
 }
 
 func userLogin(w http.ResponseWriter, r *http.Request) {
 	data := make(map[string]interface{})
 	data["Title"] = "User Login"
+	data["Username"] = "Sam Wang"
 	tpl.ExecuteTemplate(w, "user-login.gohtml", data)
 }
 
